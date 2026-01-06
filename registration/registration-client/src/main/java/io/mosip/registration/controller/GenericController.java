de6fdf7b93fbae2880ac7603b0fe2d9c2ad678ff
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-01-06 15:11:25 +0530

    Update GenericController.java

--- registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
@@ -240,15 +240,15 @@ public class GenericController extends BaseController {
 					@Override
 					protected Void call() {
 						Platform.runLater(() -> {
-							boolean isValid = false;
-							try {
-								isValid = pridValidatorImpl.validateId(textField.getText());
-							} catch (InvalidIDException invalidIDException) { isValid = false; }
+							// boolean isValid = false;
+							// try {
+							// 	isValid = pridValidatorImpl.validateId(textField.getText());
+							// } catch (InvalidIDException invalidIDException) { isValid = false; }
 
-							if(!isValid) {
-								generateAlertLanguageSpecific(RegistrationConstants.ERROR, RegistrationUIConstants.PRE_REG_ID_NOT_VALID);
-								return;
-							}
+							// if(!isValid) {
+							// 	generateAlertLanguageSpecific(RegistrationConstants.ERROR, RegistrationUIConstants.PRE_REG_ID_NOT_VALID);
+							// 	return;
+							// }
 							ResponseDTO responseDTO = preRegistrationDataSyncService.getPreRegistration(textField.getText(), false);
 							
 							if (responseDTO.getErrorResponseDTOs() != null
@@ -1541,4 +1541,5 @@ public class GenericController extends BaseController {
 		TabPane tabPane = (TabPane) anchorPane.lookup(HASH + getRegistrationDTOFromSession().getRegistrationId());
 		return tabPane.getSelectionModel().getSelectedItem().getId().replace("_tab", EMPTY);
 	}
+
 }
